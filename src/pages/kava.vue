969bfed542434759060cf9f25f9749835693bc85
sven-ra <mail@svenraju.net>
2022-02-21 11:34:34 +0200

    feat: add ui for schedule page

--- src/pages/kava.vue
+++ src/pages/kava.vue
@@ -1,25 +1,63 @@
 <script setup lang="ts">
 import { useProjects } from "../utils";
+import { useRange } from "elektro";
 
 const { upcomingProjects } = useProjects();
+console.log(upcomingProjects);
+
+const isUpcoming = (start_at: string, end_at: string) => {
+  const { urgency } = useRange(new Date(start_at), new Date(end_at));
+  return urgency.value === "future";
+};
 </script>
 
 <template>
-  <EStack class="Page">
-    <template v-for="project in upcomingProjects">
-      <ETitle size="lg" v-html="project.title" />
-      <EContent v-html="project.intro" />
-      <template v-for="event in project.upcomingEvents">
-        <ETitle v-html="event.title" />
-        <p>{{ event.formattedFromDatetime }} / {{ event.formattedDistance }}</p>
-        <a :href="event.liveUrl" target="_blank">{{ event.liveUrl }}</a>
-      </template>
+  <main class="Page Projects">
+    <template
+      v-if="upcomingProjects.length > 0"
+      v-for="project in upcomingProjects"
+    >
+      <EScheduleEvent
+        :title="project.title"
+        :description="project.intro"
+        :path="''"
+      >
+        <!-- @TODO: Remove this once router is figured out -->
+        <ETitle size="lg" :title="project.title" />
+        <template v-if="project.events" v-for="event in project.events">
+          <EEventInstance
+            client:only
+            v-if="isUpcoming(event.start_at, event.end_at)"
+            :start-at="event.formattedFromDatetime"
+            :end-at="event.formattedDistance"
+            :ticket-url="event.liveUrl"
+          />
+        </template>
+      </EScheduleEvent>
     </template>
-  </EStack>
+    <section v-else>
+      <ETitle>Tulevaid sündmusi ei leitud</ETitle>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-.Page {
-  padding: var(--p-5);
+.Page.Projects {
+  display: grid;
+  grid-template-columns: 1fr;
+  padding: var(--p-4);
+}
+.Page.Projects > section {
+  margin-bottom: var(--m-12);
+}
+/* @TODO: Add breakpoints system */
+@media only screen and (min-width: 1240px) {
+  .Page.Projects {
+    grid-template-columns: repeat(8, 1fr);
+  }
+  .Page.Projects > section {
+    grid-column: 4 / -1;
+    margin-bottom: var(--m-24);
+  }
 }
 </style>
