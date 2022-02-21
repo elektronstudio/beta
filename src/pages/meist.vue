4af59a883f97c2cc3edcea44475ff42ab6770d41
sven-ra <mail@svenraju.net>
2022-02-21 16:03:36 +0200

    feat: update about page data

--- src/pages/meist.vue
+++ src/pages/meist.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { getAboutPage } from "../utils";
 import ContentBlocks from "../components/ContentBlocks.vue";
 
-const cards = ref<any>(null);
-const data = await getAboutPage();
-console.log(data);
-cards.value = data.data.attributes.cards;
+const { data } = await getAboutPage();
 </script>
 
 <template>
   <main class="Page About">
-    <ContentBlocks v-if="cards" :cards="cards" />
+    <ContentBlocks
+      v-if="data.attributes.cards"
+      :cards="data.attributes.cards"
+    />
   </main>
 </template>
