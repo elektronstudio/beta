fcd740e176a87c265ff841e196d6e69c426d0737
sven-ra <mail@svenraju.net>
2022-02-21 15:59:57 +0200

    chore: add hardcoded strapi v4 variable

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,7 @@
 export const config = {
   strapiUrl: import.meta.env.VITE_STRAPI_URL || "",
-  strapiV4Url: import.meta.env.VITE_STRAPI_V4_URL || "",
+  // @TODO: remove hardcoded variable
+  strapiV4Url:
+    import.meta.env.VITE_STRAPI_V4_URL || "https://strapi-v4-ee9v.onrender.com",
   liveUrl: import.meta.env.VITE_LIVE_URL || "",
 };
