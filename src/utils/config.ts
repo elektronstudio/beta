c6f11f0103adffd0b00f3b1f60ee6c2c26e744ca
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-02-28 18:44:52 +0200

    feat: fixed fienta button (#3)

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,4 +4,5 @@ export const config = {
   strapiV4Url:
     import.meta.env.VITE_STRAPI_V4_URL || "https://strapi-v4-ee9v.onrender.com",
   liveUrl: import.meta.env.VITE_LIVE_URL || "",
+  fientaTicketUrl: import.meta.env.VITE_FIENTA_TICKET_URL || "",
 };
