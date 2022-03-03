86bb674c35e1963a32ef751bd3602404515a3b48
kristjanjansen <kristjan.jansen@gmail.com>
2022-03-03 22:40:40 +0200

    hm

--- src/pages/projects/[project_slug]/index.vue
+++ src/pages/projects/[project_slug]/index.vue
@@ -5,11 +5,10 @@ type Props = {
   project_slug: string;
 };
 const { project_slug } = defineProps<Props>();
-const project: any = useProjectBySlug(project_slug);
+const project = useProjectBySlug(project_slug);
 </script>
 
 <template>
   <pre>Project {{ project_slug }}</pre>
-  <RouterLink v-for="e in project.events" :to="/">Link</RouterLink>
   <pre>{{ project }}</pre>
 </template>
