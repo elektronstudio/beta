ec9f2875d6109f0868b84d9b9ae65b9841e3a6c6
kristjanjansen <kristjan.jansen@gmail.com>
2022-03-05 18:11:11 +0100

    cleanup

---
+++ src/components/Chat.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+// TODO: Move back to elektro
+import { EButton, useChat } from "elektro";
+
+type Props = {
+  channel: string;
+};
+
+const { channel } = defineProps<Props>();
+
+const {
+  chatMessages,
+  newChatMessage,
+  onNewChatMessage,
+  scrollRef,
+  textareaRef,
+} = useChat(channel);
+</script>
+
+<template>
+  <div class="EChat">
+    <div class="messages" ref="scrollRef">
+      <template v-for="message in chatMessages">
+        <div v-if="message.value" class="message">
+          <p class="username">Username</p>
+          <p>{{ message.value }}</p>
+        </div>
+      </template>
+    </div>
+    <div class="messageBox">
+      <!-- TODO: Make it work with ref -->
+      <textarea
+        class="chatTextarea"
+        v-model="newChatMessage"
+        ref="textareaRef"
+        resize="none"
+      />
+      <EButton size="xs" color="accent" @click="onNewChatMessage"
+        >Saada</EButton
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.EChat {
+  display: flex;
+  flex-direction: column;
+  padding: var(--p-2);
+  background-color: var(--bg);
+  /* @TODO: Is this too much? */
+  width: 100%;
+  height: 100%;
+}
+
+.messages {
+  flex-grow: 1;
+  overflow-y: scroll;
+}
+
+.message {
+  padding: var(--p-3);
+  font-family: var(--font-mono);
+  font-size: var(--text-xs);
+}
+
+.username {
+  color: var(--gray-400);
+}
+
+/* TODO: add admin message class modifier */
+.message:nth-child(4) {
+  background-color: var(--accent);
+}
+.message:nth-child(4) p {
+  color: var(--bg);
+}
+.message:nth-child(4) {
+  background-color: var(--accent);
+}
+
+.messageBox {
+  flex-shrink: 0;
+  margin-bottom: 0;
+  /* height: 2rem; */
+  display: flex;
+  align-items: flex-end;
+}
+
+.chatTextarea {
+  flex-grow: 1;
+  margin-right: var(--p-2);
+  background-color: var(--bg);
+  padding: 0 var(--p-3);
+  resize: none;
+  height: 1.6rem;
+  border: 1px solid var(--gray-300);
+  width: 100%;
+}
+
+.chatTextarea:focus {
+  outline: none;
+  border-color: var(--fg);
+}
+.chatTextarea:not(:disabled):hover {
+  background-color: rgba(250, 250, 250, 0.1);
+}
+
+/* .chatTextarea:hover {
+  outline: 1px solid var(--gray-500);
+}
+.chatTextarea:focus {
+  outline: none;
+  background-color: var(--gray-500);
+} */
+
+.EButton {
+  /* margin-bottom: var(--m-2); */
+}
+</style>
--- src/pages/projects/[project_slug]/[event_slug]/live/index.vue
+++ src/pages/projects/[project_slug]/[event_slug]/live/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useEventBySlug } from "@/utils";
-import { useChat, useVideostream } from "elektro";
+import { useChat } from "elektro";
 import { computed, ref } from "vue";
 
 type Props = {
@@ -12,50 +12,24 @@ const event = useEventBySlug(event_slug);
 
 // TODO: support multiple videos
 const stream = computed(() => event?.value.videostreams[0]);
-
-const {
-  chatMessages,
-  newChatMessage,
-  onNewChatMessage,
-  scrollRef,
-  textareaRef,
-} = useChat(event?.value.slug);
 </script>
 
 <template>
-  <div v-if="event">
+  <div v-if="event" class="Live">
     <RouterLink :to="event.route">&larr; Back to event</RouterLink>
     <Videostream :src="stream.streamurl">
       <div>Viewers: {{ stream.viewers }}</div>
     </Videostream>
     <ETitle size="lg">Live event: {{ event.title }}</ETitle>
     <EContent v-html="event.intro" />
-    <div ref="scrollRef" class="messages">
-      <EBox v-for="message in chatMessages">
-        <pre>{{ message }}</pre>
-      </EBox>
-    </div>
-    <EFormTextArea v-model="newChatMessage" />
-    <EButton size="xs" @click="onNewChatMessage">Submit</EButton>
+    <Chat :channel="event_slug" />
   </div>
 </template>
 
 <style scoped>
 .Live {
   display: grid;
-  grid-template-columns: 2fr 1fr;
   gap: var(--gap-5);
   padding: var(--p-5);
-}
-.Live > * {
-  display: grid;
-  grid-auto-rows: min-content;
-  gap: var(--gap-5);
-}
-.messages {
-  height: 60vh;
-  display: grid;
-  gap: var(--gap-4);
-  overflow: auto;
 }
 </style>
