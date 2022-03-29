538bdba66e2dd2989897bd6fda8a7492876a1191
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-03-29 16:50:43 +0300

    fix: chat url fixes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import routes from "virtual:generated-pages";
 import App from "./App.vue";
 import "elektro/style.css";
+import { config } from "elektro";
+
+// TODO: Move to createConfig() function and unify with art config
+
+config.wsUrl = import.meta.env.VITE_WS_URL as string;
+config.messagesUrl = import.meta.env.VITE_MESSAGES_URL as string;
+config.strapiUrl = import.meta.env.VITE_STRAPI_URL as string;
+config.fientaUrl = import.meta.env.VITE_FIENTA_URL as string;
+config.fientaToken = import.meta.env.VITE_FIENTA_TOKEN as string;
+config.fientaTicketUrl = import.meta.env.VITE_FIENTA_TICKET_URL as string;
 
 const router = createRouter({
   history: createWebHistory(),
