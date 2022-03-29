8f69f2f2194643ca3ec3a4c2386ec50b15194ab0
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-03-29 18:12:19 +0300

    feat: video controls

--- src/components/Videostream.vue
+++ src/components/Videostream.vue
@@ -1,35 +1,42 @@
 <script setup lang="ts">
 // TODO: Move to elektro
+import IconMuted from "~icons/radix-icons/speaker-off";
+import IconUnmuted from "~icons/radix-icons/speaker-loud";
 import { useVideostream } from "elektro";
+import { ref } from "vue";
 
 type Props = {
   src: string;
 };
 const { src } = defineProps<Props>();
 const { videoRef, width, height, status } = useVideostream(src);
+const muted = ref(false);
 </script>
 
 <template>
   <div style="position: relative">
     <video
       ref="videoRef"
-      muted
+      :muted="muted"
       autoplay
       controls
       :width="width"
       :height="height"
       style="width: 100%"
     />
-    <div
-      style="
-        position: absolute;
-        top: var(--p-2);
-        left: var(--p-2);
-        font-family: var(--font-mono);
-        font-size: var(--text-xs);
-      "
-    >
+    <div style="position: absolute; top: var(--p-4); left: var(--p-4)">
       <slot />
+    </div>
+    <div style="position: absolute; top: var(--p-4); right: var(--p-4)">
+      <EButton
+        v-if="muted"
+        size="xs"
+        color="transparent"
+        @click="muted = !muted"
+      >
+        <IconMuted />
+        Click to unmute
+      </EButton>
     </div>
   </div>
 </template>
