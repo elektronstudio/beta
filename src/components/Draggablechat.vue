9cbf8352f07c72998184c7272950ca7733e9e6cf
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-30 09:02:15 +0300

    feat: draggable chat (#30)

---
+++ src/components/Draggablechat.vue
@@ -0,0 +1,204 @@
+<script setup lang="ts">
+// TODO: Move to elektro
+
+import { computed, Ref, ref, watchEffect } from "vue";
+import { debouncedWatch, useDraggable, useWindowSize } from "@vueuse/core";
+import { useMessage, safeJsonParse } from "elektro";
+import type { Message } from "elektro";
+import { useStorage } from "@vueuse/core";
+import { randomString, EFormTextArea } from "elektro";
+import { useMagicKeys } from "@vueuse/core";
+
+const { ws, sendMessage } = useMessage();
+
+type DraggableChatUser = {
+  userId: string;
+  userName: string;
+  x: number;
+  y: number;
+  chat: string;
+};
+
+const UPDATE_RATE = 600; // TODO: Make it a function of user count
+const ANIMATION_RATE = 300;
+// https://cubic-bezier.com/#.48,.76,.78,.95
+const ANIMATION_EASING = "cubic-bezier(.48,.76,.78,.95)";
+
+function useDraggableChat(
+  channel: string,
+  userId: Ref<string>,
+  userName: Ref<string>,
+) {
+  const users = ref<DraggableChatUser[]>([]);
+
+  ws.addEventListener("message", ({ data }) => {
+    const message = safeJsonParse(data);
+    if (message.channel === channel && message.type === "DRAGGABLECHAT") {
+      const user = {
+        userId: message.userId,
+        userName: message.userName,
+        x: message.value.x,
+        y: message.value.y,
+        chat: message.value.chat,
+      };
+      const existingUserIndex = users.value?.findIndex((u) => {
+        return u.userId === user.userId;
+      });
+      if (users.value && existingUserIndex > -1) {
+        users.value[existingUserIndex] = user;
+      } else {
+        users.value = [...users.value, user];
+      }
+    }
+  });
+
+  const userRef = ref<HTMLElement | null>(null);
+
+  const {
+    x,
+    y,
+    style: userStyle,
+  } = useDraggable(userRef, {
+    // TODO: Initialize with random values
+    initialValue: { x: 100, y: 100 },
+  });
+
+  const { width, height } = useWindowSize();
+  const center = computed(() => {
+    return { x: width.value / 2, y: height.value / 2 };
+  });
+
+  const chat = ref("");
+
+  debouncedWatch(
+    [x, y, chat],
+    () => {
+      const message: Message = {
+        channel,
+        type: "DRAGGABLECHAT",
+        userId: userId.value,
+        userName: userName.value,
+        value: {
+          x: x.value - center.value.x,
+          y: y.value - center.value.y,
+          chat: chat.value,
+        },
+      };
+      sendMessage(message);
+      // TODO: Store user locaction locally?
+    },
+    {
+      immediate: true,
+      debounce: UPDATE_RATE,
+    },
+  );
+
+  const otherUsers = computed(() =>
+    users.value.filter((u) => u.userId !== userId.value),
+  );
+
+  const otherUserStyle = (user: DraggableChatUser) => {
+    const x = user.x + center.value.x;
+    const y = user.y + center.value.y;
+    return {
+      top: `${y}px`,
+      left: `${x}px`,
+      transition: `all ${ANIMATION_RATE}ms ${ANIMATION_EASING}`,
+    };
+  };
+  // TODO: Remove _users when done debugging
+  return {
+    center,
+    userRef,
+    userStyle,
+    otherUsers,
+    otherUserStyle,
+    chat,
+    _users: users,
+  };
+}
+
+// ---
+
+const userId = useStorage("elektron_user_id", randomString());
+const userName = useStorage("elektron_user_name", randomString());
+
+const { userRef, userStyle, otherUsers, otherUserStyle, chat } =
+  useDraggableChat("draggablechat", userId, userName);
+
+const enabled = ref(false);
+const { meta, shift, c } = useMagicKeys();
+watchEffect(() => {
+  if (shift.value && c.value) {
+    enabled.value = !enabled.value;
+  }
+});
+</script>
+
+<template>
+  <div v-if="enabled">
+    <div
+      style="
+        background: rgba(0, 0, 0, 0.7);
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+      "
+    />
+    <div
+      v-for="user in otherUsers"
+      :style="otherUserStyle(user)"
+      style="position: fixed; display: flex; gap: var(--gap-2); width: 200px"
+    >
+      <div
+        style="
+          width: 20px;
+          height: 20px;
+          background: white;
+          border-radius: 10000px;
+          flex-shrink: 0;
+          opacity: 0.5;
+        "
+      />
+      <div style="pointer-events: none; user-select: none">
+        <div style="font-size: var(--text-xs); opacity: 0.3">
+          {{ user.userName }}
+        </div>
+        <div style="opacity: 0.6">{{ user.chat }}</div>
+      </div>
+    </div>
+    <div
+      ref="userRef"
+      :style="userStyle"
+      style="position: fixed; display: flex; gap: var(--gap-2); width: 200px"
+    >
+      <div
+        style="
+          width: 20px;
+          height: 20px;
+          background: rgba(255, 0, 0, 0.7);
+          border-radius: 10000px;
+          flex-shrink: 0;
+        "
+      />
+      <div>
+        <div style="font-size: var(--text-xs); opacity: 0.5">
+          {{ userName }}
+        </div>
+        <div style="letter-spacing: 0.04em">{{ chat }}</div>
+      </div>
+    </div>
+    <div
+      style="
+        position: fixed;
+        left: var(--p-4);
+        bottom: var(--p-4);
+        width: 300px;
+      "
+    >
+      <EFormTextArea v-model="chat" />
+    </div>
+  </div>
+</template>
